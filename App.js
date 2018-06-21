c366ecf8acdb931c52e4fca681b5233ac45d1022
Parth <vermaparth97@gmail.com>
2018-06-21 10:05:00 +0200

    Files added

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default class App extends Component {
+  state = {
+    placeName: ""
+  }
+  placeNameChangeHandler = event => {
+    alert(event);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput 
+        style={{width: 300, borderColor: "black", borderWidth: 1}}
+        value={this.state.placeName} 
+        onChangeText={this.placeNameChangeHandler} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
