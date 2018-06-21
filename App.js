8648ef5bd205c4735cc6ec2be47ba0e79427aae5
Parth <vermaparth97@gmail.com>
2018-06-21 10:41:59 +0200

    Added listing

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends Component {
   state = {
-    placeName: ""
+    placeName: "",
+    places: []
   }
-  placeNameChangeHandler = event => {
-    alert(event);
+  placeNameChangeHandler = val => {
+    this.setState({
+      placeName: val
+    })
+  }
+
+  placeSubmitHandler = () => {
+    if(this.state.placeName.trim() === ""){
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      }
+    })
   }
   render() {
+  const placesOutput = this.state.places.map((place,i) => <Text key={i}>{place}</Text>)
     return (
       <View style={styles.container}>
-        <TextInput 
-        style={{width: 300, borderColor: "black", borderWidth: 1}}
-        value={this.state.placeName} 
-        onChangeText={this.placeNameChangeHandler} />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={{ width: 300 }}
+            placeholder="Awesome place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangeHandler}
+            style={styles.placeInput} />
+          <Button title="Add" style={styles.placeButton} onPress={this.placeSubmitHandler}/>
+        </View>
+        <View>
+          {placesOutput}
+        </View>
       </View>
     );
   }
@@ -23,8 +47,22 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 26,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  inputContainer: {
+    //flex: 1,
+    width:"100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
 });
