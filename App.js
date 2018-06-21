115ab07f3b4ee80083d86dad6d0af6d5ada9c233
Parth <vermaparth97@gmail.com>
2018-06-21 21:13:53 +0200

    Made more components

--- App.js
+++ App.js
@@ -1,42 +1,27 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import ListItem from './src/components/ListItem'
-import HandleInput from './src/components/HandleInput';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+
 export default class App extends Component {
   state = {
-    placeName: "",
     places: []
-  }
-  placeNameChangeHandler = val => {
-    this.setState({
-      placeName: val
-    })
-  }
+  };
 
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
-
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(prevState.placeName)
-      }
-    })
-  }
+        places: prevState.places.concat(placeName)
+      };
+    });
+  };
+
   render() {
-    const placesOutput = this.state.places.map((place, i) => (
-      <ListItem key={i} placeName={place} />
-    ));
     return (
       <View style={styles.container}>
-        <HandleInput
-          placeName={this.state.placeName}
-          placeNameChangeHandler={this.placeNameChangeHandler}
-          placeSubmitHandler={this.placeSubmitHandler} />
-        <View style={styles.listContainer}>
-          {placesOutput}
-        </View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places} />
       </View>
     );
   }
@@ -46,12 +31,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 26,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  listContainer: {
-    width: "100%"
-  },
-
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
 });
